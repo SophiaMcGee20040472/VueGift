5ce18a05df83c0922ecc950578fbc0d784e96f96
SophiaMcGee20040472 <83893260+SophiaMcGee20040472@users.noreply.github.com>
2025-10-27 23:23:13 +0000

    updating router

--- vue-gift-app/src/router/index.js
+++ vue-gift-app/src/router/index.js
@@ -1,22 +1,40 @@
-// src/router/index.js
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Categories from "../views/Categories.vue";
 import Trending from "../views/Trending.vue";
 import GiftIdeas from "../views/GiftIdeas.vue";
 import Contact from "../views/Contact.vue";
-import GiftIdeas from "../views/GiftIdeas.vue";
 
 const routes = [
-  { path: "/", component: Home },
-  { path: "/categories", component: Categories },
-  { path: "/trending", component: Trending },
-  { path: "/gift-ideas", component: GiftIdeas },
-  { path: "/contact", component: Contact },
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/categories",
+    name: "Categories",
+    component: Categories,
+  },
+  {
+    path: "/trending",
+    name: "Trending",
+    component: Trending,
+  },
+  {
+    path: "/gift-ideas",
+    name: "GiftIdeas",
+    component: GiftIdeas,
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    component: Contact,
+  },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
 
